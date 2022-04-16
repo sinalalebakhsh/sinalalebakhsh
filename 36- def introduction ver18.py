521b265dc5f0cb18eccd400ad9dca57a7ec58ec6
sina lalebakhsh <93151188+sinalalebakhsh@users.noreply.github.com>
2022-04-16 13:51:08 +0430

    مرور مطالب مهم همراه با output

---
+++ 36- def introduction ver18.py
@@ -0,0 +1,22 @@
+def import_os():
+    import os
+    os.system('cls')
+import_os()
+
+def addition_of_numbers(*args, **kwargs):
+    dict_args = []
+    for s in args:
+        dict_args.append(s)
+    print(dict_args) #return list
+    print(args) #return tuple
+    print(kwargs) #return dictionary 
+    print(kwargs.items()) #return list in tuple
+    list_kwargs = []
+    for s in kwargs.items():
+        list_kwargs.append(s)
+    print(list_kwargs) #return list of dictiomary ex
+    for name_,value_ in list_kwargs:
+        print(name_, value_, end='|') #return tuple unpaking
+        
+
+addition_of_numbers(1,2,3,4,a='sina',b=10,c='mahsa',d=20)
